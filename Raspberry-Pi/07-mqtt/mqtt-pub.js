6033992f65da94e95b8e9d37d723a204d137aab9
zeldi <zeldi@leafbyte.com>
2018-11-14 01:09:47 +0800

    modified mqtt-pub.js

--- Raspberry-Pi/07-mqtt/mqtt-pub.js
+++ Raspberry-Pi/07-mqtt/mqtt-pub.js
@@ -15,8 +15,8 @@ client.on("connect",function(){
 
         // start reading data from sensor GPIO after connection to MQTT Broker
         var datastream =  readDHT11();
-        JSON.stringify(datastream);
-        client.publish('sensor/dht11',datastream);
+        
+        client.publish('sensor/dht11',JSON.stringify(datastream));
 })
 
 
