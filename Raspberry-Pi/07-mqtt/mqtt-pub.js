e5d15d2f88e62c59de8f57fe441dfb72e79fa555
zeldi <zeldi@leafbyte.com>
2018-11-14 00:51:52 +0800

    modified mqtt

--- Raspberry-Pi/07-mqtt/mqtt-pub.js
+++ Raspberry-Pi/07-mqtt/mqtt-pub.js
@@ -1,4 +1,6 @@
 var mqtt    = require('mqtt');
+var sensor = require('node-dht-sensor');
+
 var client  = mqtt.connect("mqtt:m15.cloudmqtt.com",{username:"tbtnsrke",password:'M93l7HPVUSiu',port:15704});
 
 console.log("MQTT Connection flag = " + client.connected);
@@ -10,12 +12,23 @@ client.on('message',function(topic, message, packet){
 
 client.on("connect",function(){
         console.log("MQTT Connected flag =  "+ client.connected);
-        var json = {
-                    sensor_id : "temperature",
-                    value : 100
-                    };
-            // Convert the JSON object to a string
-        var str = JSON.stringify(json);
 
-        client.publish('sensor/testsensor',str);
+        // start reading data from sensor GPIO after connection to MQTT Broker
+        var datastream =  readDHT11();
+
+        client.publish('sensor/dht11',datastream);
 })
+
+
+var readDHT11 = function() {
+        var result = sensor.read(11,17);
+        console.log('temp: ' + result.temperature.toFixed(1) + ' Celcius');
+        console.log('humidity: ' + result.humidity.toFixed(1) + ' %');
+     
+        var sensorData = {
+           temp : result.temperature.toFixed(1),
+           hum : result.humidity.toFixed(1)
+        }
+     
+        return sensorData;	
+}
--- Raspberry-Pi/07-mqtt/mqtt-sub.js
+++ Raspberry-Pi/07-mqtt/mqtt-sub.js
@@ -1,21 +1,54 @@
+var request = require("request");
 var mqtt    = require('mqtt');
+
 var client  = mqtt.connect("mqtt:m15.cloudmqtt.com",{username:"tbtnsrke",password:'M93l7HPVUSiu',port:15704});
 
 console.log("MQTT Connection flag = " + client.connected);
 
+client.on("connect",function(){
+        console.log("MQTT Connected flag =  "+ client.connected);
+        client.subscribe('sensor/dht11');
+})
+
 //handle incoming messages
 client.on('message',function(topic, message, packet){
-        console.log("message is "+ message);
         console.log("topic is "+ topic);
         console.log("message is " + message);
 
-        jsonMsg = JSON.parse(message);
-        console.log(jsonMsg.sensor_id);
-        // client.end();
+        // Parse message to JSON
+        dhtData = JSON.parse(message);
+        // send data stream to IOT Platform
+        send_stream(dhtData);
 });
 
 
-client.on("connect",function(){
-        console.log("MQTT Connected flag =  "+ client.connected);
-        client.subscribe('sensor/testsensor');
-})
+var send_stream = function(data){
+        var device = 'AP_1@zeldi.suryady' //"YOUR_ID_DEVELOPER_DEVICE"
+        var apikey = "66c1a5d2c2d8546ef20295bf3d8824467aafec8cb22525432c8d77244aa79447" //"YOUR APIKEY"
+    
+        var options = { 
+          method: 'POST',
+          url: 'http://api.altairsmartcore.com/streams/',
+          headers: 
+           { 
+             'cache-control': 'no-cache',
+             'carriots.apikey': apikey,
+             'Content-Type': 'application/json' },
+          body: 
+           { protocol: 'v2',
+             checksum: '',
+             device: device,
+             at: 'now',
+             data: data //{ temp: '26', hum: '40%' } 
+            },
+          json: true 
+        };
+    
+        request(options, function (error, response, body) {
+          if (error) throw new Error(error);
+    
+          console.log(body);
+        });
+    
+};
+
